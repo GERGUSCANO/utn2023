c76a49955d5e09a968ca406e1f6f76fe9c140613
german <germangustavocano@gmail.com>
2023-11-14 18:52:06 -0300

    se suben cambios en la clase 1

--- ejercicios utn/clase 1/js/clase1.js
+++ ejercicios utn/clase 1/js/clase1.js
@@ -1,6 +1,24 @@
-function actualizarEstado() {
+//PROBANDO VARIAS MANERAS DE HACER
+/*function actualizarEstado() {
     var estadoSelect = document.getElementById("estado");
     var estadoValue = estadoSelect.options[estadoSelect.selectedIndex].value;
     console.log("Estado seleccionado: " + estadoValue);
 }
-actualizarEstado();
+actualizarEstado();*/
+
+//LOS PROFESORES PREFIEREN ESTA MANERA 
+/*const selectStatus = document.getElementById('estado');
+const estadoSeleccionado = document.getElementById('estadoSeleccionado');
+
+selectStatus.addEventListener('change', (e) => {
+    estadoSeleccionado.value = e.target.value;
+})*/
+//ENTIENDO ESTA MANERA
+function cambiarEstado(){
+    const select= document.getElementById('estado');
+    const estadoSeleccionado=document.getElementById('estadoSeleccionado');
+    const opcionSeleccionada=select.value;
+
+    estadoSeleccionado.textContent = opcionSeleccionada;
+
+}
